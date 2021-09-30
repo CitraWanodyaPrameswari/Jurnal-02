d31c2911ad3bb14ee2d352103a346db750a1c58e
CitraWanodyaPrameswari <ciwari@student.telkomuniversity.ac.id>
2021-09-30 15:17:47 +0700

    HelloWorld.java

---
+++ DemoGit/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+public static void main(String []args) {
+System.out.println("Citra Wanodya Prameswari!");
+System.out.println("6706213094");
+System.out.println("D3 RPLA 45-05");
+
+}
+}
